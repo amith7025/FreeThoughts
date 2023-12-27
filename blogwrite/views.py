0f28a20e05b423911449175814d90c124bcaeab3
AMITH E K <amithajith7025@gmail.com>
2023-12-27 16:13:38 +0530

    almost completed

--- blogwrite/views.py
+++ blogwrite/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import BlogModelForm
+from .models import Blog
 
 # Create your views here.
 
 def blogwrite(request):
+    if request.method == 'POST':
+        god_title = request.POST.get('title')
+        god_content = request.POST.get('content')
+
+        Blog.objects.create(title=god_title,content=god_content)
+        return render(request,'blogwrite.html')
+
+
     return render(request,'blogwrite.html')
